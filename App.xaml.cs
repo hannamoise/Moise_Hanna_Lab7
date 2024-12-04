0c849261f0a14ab9bf4133e12ecbc061458f620f
Hanna Moise <hannamoise@gmail.com>
2024-12-04 20:14:34 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace Moise_Hanna_Lab7
+﻿using System;
+using Moise_Hanna_Lab7.Data;
+using System.IO;
+
+
+namespace Moise_Hanna_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
